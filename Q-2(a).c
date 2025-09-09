a39026fc178ecee62473ed9d8a53160dc86bfe2b
pratit8910 <pratitpatel8910@gmail.com>
2025-09-09 10:02:16 +0530

    Add files via upload

---
+++ Q-2(a).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num[10];
+    for(int i=0;i<10;i++)
+    {
+       printf("ENTER NUMBER:");
+       scanf("%d",&num[i]);
+    }
+printf("%d\n%d\n%d\n",num[3],num[6],num[8]);
+
+
+
+}
